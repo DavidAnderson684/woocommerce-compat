e35f3c4f66fff1e0c1ab2bb29fd9a0806f926a05
David Anderson <DavidAnderson684@users.noreply.github.com>
2017-01-28 16:57:27 +0000

    add a get_id method

--- woocommerce-compat.php
+++ woocommerce-compat.php
@@ -16,6 +16,13 @@ Licenced according to the MIT Licence; see the file LICENCE
 
 class WooCommerce_Compat_1_0 {
 
-
+	/**
+	 * Get the ID of a passed object
+	 *
+	 * @param object $object Any WooCommerce object that has an ID (available via either a get_id() method or an id property)
+	 */
+	public function get_id($object) {
+		return method_exists($object, 'get_id') ? $object->get_id() : $object->id;
+	}
 
 }
