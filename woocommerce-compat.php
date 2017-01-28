041fb5570558123add803447cc6026c39d811b50
David Anderson <DavidAnderson684@users.noreply.github.com>
2017-01-28 16:09:28 +0000

    initial commit

---
+++ woocommerce-compat.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!defined('ABSPATH')) die('No direct access.');
+
+/*
+
+WooCommerce compatibility library.
+
+Get full/current info at:
+https://github.com/DavidAnderson684/woocommerce-compat/
+
+Copyright David Anderson 2017-
+Licenced according to the MIT Licence; see the file LICENCE
+
+*/
+
+class WooCommerce_Compat_1_0 {
+
+
+
+}
